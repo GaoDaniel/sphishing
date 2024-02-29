57bf71962664ef759878a928cc2eb1e85c938a10
Daniel Pyon <danielpyon@Daniels-Macbook-Pro.local>
2024-02-28 22:38:04 -0800

    create stats struct

--- web/main.go
+++ web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,7 +11,40 @@ import (
 	"github.com/google/uuid"
 )
 
-var mu sync.Mutex
+// TODO: support finetune / non-finetune
+
+type StatsInner struct {
+	Filename      string          `json:"filename"`
+	Total         int             `json:"total"`
+	Clicks        int             `json:"clicks"`
+	RealismScores map[string]int  `json:"realismScores"`
+	Codes         map[string]bool `json:"codes"`
+}
+
+type Stats struct {
+	Stats StatsInner
+	Lock  sync.Mutex
+}
+
+func (s *Stats) Clicks() (int, error) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	if err := s.load(); err != nil {
+		return 0, err
+	}
+
+	return s.Stats.Clicks, nil
+}
+
+func (s *Stats) Clickthrough() (float32, error) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+	if err := s.load(); err != nil {
+		return 0, err
+	}
+	return float32(s.Stats.Clicks) / float32(s.Stats.Total), nil
+}
 
 func generateCodes(n int) []string {
 	codes := make([]string, n)
@@ -20,63 +54,144 @@ func generateCodes(n int) []string {
 	return codes
 }
 
-func writeCodes() {
-	mu.Lock()
-	defer mu.Unlock()
-
-	codes, err := json.Marshal(generateCodes(20))
-	if err != nil {
-		panic("could not generate codes")
+// create new Stats struct stored in filename
+func NewStats(filename string, total int) (*Stats, error) {
+	s := &Stats{
+		Stats: StatsInner{
+			Filename:      filename,
+			Clicks:        0,
+			Total:         total,
+			RealismScores: make(map[string]int),
+			Codes:         make(map[string]bool),
+		},
 	}
 
-	if os.WriteFile("codes.json", codes, 0644) != nil {
-		panic("could not write to file")
+	codes := generateCodes(total)
+
+	for _, code := range codes {
+		s.Stats.RealismScores[code] = -1
+		s.Stats.Codes[code] = true
 	}
+
+	if err := s.flush(); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
-func redeem(code string) bool {
-	mu.Lock()
-	defer mu.Unlock()
-
-	var codes []string
-
-	data, err := os.ReadFile("codes.json")
+// write stats to disk
+// must hold lock!
+func (s *Stats) flush() error {
+	serialized, err := json.Marshal(&s.Stats)
 	if err != nil {
-		panic("could not read file")
+		return err
 	}
 
-	if err = json.Unmarshal(data, &codes); err != nil {
-		panic("could not unmarshal json")
+	if err = os.WriteFile(s.Stats.Filename, serialized, 0644); err != nil {
+		return err
 	}
 
-	var updatedCodes []string
-	found := false
-	for _, c := range codes {
-		if c != code {
-			updatedCodes = append(updatedCodes, c)
-		} else {
-			found = true
-		}
-	}
+	return nil
+}
 
-	ucBytes, err := json.Marshal(updatedCodes)
+// must hold lock!
+func (s *Stats) load() error {
+	data, err := os.ReadFile(s.Stats.Filename)
 	if err != nil {
-		panic("could not marshal json")
+		return err
 	}
 
-	if os.WriteFile("codes.json", ucBytes, 0644) != nil {
-		panic("could not write to file")
+	if err = json.Unmarshal(data, &s.Stats); err != nil {
+		return err
 	}
 
-	return found
+	return nil
+}
+
+// returns whether code exists
+func (s *Stats) Redeem(code string) (bool, error) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	if err := s.load(); err != nil {
+		return false, err
+	}
+
+	redeemed, present := s.Stats.Codes[code]
+	if !present {
+		return false, nil
+	} else if redeemed {
+		// redeem the code
+		s.Stats.Codes[code] = false
+		s.Stats.Clicks++
+	}
+
+	if err := s.flush(); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// realism score
+func (s *Stats) Score(code string) (int, error) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	if err := s.load(); err != nil {
+		return 0, err
+	}
+
+	score, present := s.Stats.RealismScores[code]
+	if !present {
+		return 0, errors.New("code does not exist")
+	}
+
+	return score, nil
+}
+
+func (s *Stats) SetScore(code string, score int) error {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	if err := s.load(); err != nil {
+		return err
+	}
+
+	_, present := s.Stats.RealismScores[code]
+	if !present {
+		return errors.New("code does not exist")
+	}
+
+	s.Stats.RealismScores[code] = score
+	if err := s.flush(); err != nil {
+		return err
+	}
+
+	return nil
+
 }
 
 func main() {
-	// writeCodes()
-	fmt.Println(redeem("eb0cf47a-8350-455d-98df-72f9f0d412c2"))
+	// TODO: in actual deployment, make two stats objects (one for finetune, one for base)
+	stats, err := NewStats("codes.json", 40)
+	if err != nil {
+		panic("failed to create stats struct")
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	http.HandleFunc("/redeem", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		fmt.Printf("redeeming %s...\n", code)
+		found, err := stats.Redeem(code)
+
+		if err != nil {
+			fmt.Fprintln(w, "failed to redeem")
+		} else {
+			fmt.Printf("found: %t\n", found)
+			fmt.Fprintln(w, found)
+		}
 	})
+
 	http.ListenAndServe(":80", nil)
 }
